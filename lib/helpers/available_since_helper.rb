f91b22ba44d75447b2c0c7140801bd1ff4528e54
Kritika Singh <kritika.singh1994@gmail.com>
2019-04-18 16:03:54 +0530

    Documenting Stage History API v1. Added a helper method for stating when was the new version introduced.

--- lib/helpers/available_since_helper.rb
+++ lib/helpers/available_since_helper.rb
@@ -2,22 +2,38 @@ module AvailableSinceHelper
 
   class AvailableSinceDescription
     attr_reader :version, :options, :old_versions
+
     def initialize(version, options)
-      @version = version
-      @options = options
+      @version      = version
+      @options      = options
       @old_versions = {}
     end
 
-    def in_version(version, options={})
+    def in_version(version, options = {})
       @old_versions[version] = options
     end
   end
 
-  def available_since(version, options={}, &block)
+  class UpdatedToDescription
+    attr_reader :api_version, :version
+
+    def initialize(api_version, version)
+      @api_version = api_version
+      @version     = version
+    end
+  end
+
+  def available_since(version, options = {}, &block)
     since = AvailableSinceDescription.new(version, options)
     since.instance_eval(&block) if block_given?
 
     partial('partials/available_since.md.erb', locals: {since: since})
+  end
+
+  def updated_to(api_version, version)
+    update = UpdatedToDescription.new(api_version, version)
+
+    partial('partials/updated_to.md.erb', locals: {update: update})
   end
 
 end
