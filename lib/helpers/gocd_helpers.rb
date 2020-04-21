be414913c029ef12b431dfb95c83bc0fabd31b36
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-04-21 11:51:54 +0530

    Specify GoCD Release for API removal instead of month.

--- lib/helpers/gocd_helpers.rb
+++ lib/helpers/gocd_helpers.rb
@@ -5,11 +5,22 @@ module GoCDHelpers
 
   def deprecated_api_message(opts = {})
     if [nil, 'unversioned'].include?(opts[:old_api_version])
-      concat_content("The unversioned #{opts[:api_name]} API has been deprecated in GoCD #{opts[:deprecated_in_release]}. ")
+      concat_content("The unversioned #{opts[:api_name]} API has been deprecated in GoCD release v#{opts[:deprecated_in_release]}. ")
     else
-      concat_content("The #{opts[:api_name]} API version #{opts[:old_api_version]} has been deprecated in GoCD #{opts[:deprecated_in_release]}. ")
+      concat_content("The #{opts[:api_name]} API version #{opts[:old_api_version]} has been deprecated in GoCD release v#{opts[:deprecated_in_release]}. ")
     end
-    concat_content("This version will be removed in a release scheduled for #{opts[:removal_date_and_year]}. ")
+
+    if opts[:removal_date_and_year] && opts[:removal_in_release]
+      raise "Please specify either 'removal_date_and_year' or 'removal_in_release'"
+    end
+
+    if opts[:removal_date_and_year]
+      concat_content("This version will be removed in GoCD release scheduled for #{opts[:removal_date_and_year]}. ")
+    end
+
+    if opts[:removal_in_release]
+      concat_content("This version will be removed in GoCD release v#{opts[:removal_in_release]}. ")
+    end
 
     unless [nil].include?(opts[:new_api_version])
       concat_content(link_to_versioned_api(opts[:api_section], "Version #{opts[:new_api_version]}"))
